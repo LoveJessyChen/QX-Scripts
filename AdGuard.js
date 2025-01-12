03e2944ff9407118b85240a0edc9cc4c6179c439
LoveJessyChen <48858531+LoveJessyChen@users.noreply.github.com>
2025-01-12 11:30:50 +0800

    Add files via upload

---
+++ AdGuard.js
@@ -0,0 +1,23 @@
+/*************************************
+
+项目名称：AdGuard-Safari浏览器扩展
+下载地址：https://t.cn/A6xe1oaK
+更新日期：2025-01-08
+使用声明：⚠️仅供参考，🈲转载与售卖！
+
+**************************************
+
+[rewrite_local]
+^https:\/\/mobile-api\.adguard\.org\/api\/.+\/ios_validate_receipt\/(.*?) url script-response-body https://raw.githubusercontent.com/LoveJessyChen/QX-Scripts/refs/heads/main/AdGuard.js
+
+[mitm]
+hostname = mobile-api.adguard.org
+
+*************************************/
+
+
+var ddm = JSON.parse($response.body);
+
+ddm = {"products":[{"premium_status":"ACTIVE","product_id":"com.adguard.lifetimePurchase"}]};
+
+$done({body : JSON.stringify(ddm)});
